d3ad09a9f435e7a51a608a4f75acfb9b667ce81f
“yangchaojianggit config --global user.email www.1007181167@qq.com <www.1007181167@qq.com>
2017-08-29 18:37:04 +0800

    增加说明

--- app/src/main/java/com/example/previewpicture/MyBaseQuickAdapter.java
+++ app/src/main/java/com/example/previewpicture/MyBaseQuickAdapter.java
@@ -8,7 +8,6 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.previewlibrary.ThumbViewInfo;
 
-import java.util.List;
 
 /**
  * Created by yangc on 2017/8/29.
@@ -29,6 +28,7 @@ public class MyBaseQuickAdapter extends BaseQuickAdapter<ThumbViewInfo,BaseViewH
         final ImageView thumbView = helper.getView(R.id.iv);
         Glide.with(context)
                 .load(item.getUrl())
+                .error(R.drawable.ic_iamge_zhanwei)
                 .into(thumbView);
         helper.getView(R.id.iv).setTag(R.id.iv,item.getUrl());
     }
